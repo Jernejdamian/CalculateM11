7b094417c53d7b9be40315d25f79ef383a7ca344
Damian Salawa <damiandv20@o2.pl>
2024-01-13 19:36:46 +0100

    add currencies

---
+++ src/currencies.js
@@ -0,0 +1,17 @@
+export const currencies =[
+    {
+        short: "USD",
+        name: "Dolar amerykański",
+        rate: 4.4378,
+    },
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.9384,
+    },
+    {
+        short: "GBP",
+        name: "Dolar brytyjski",
+        rate: 3.9058,
+    },
+];
